a4150b1f137b51ecbfe2dbb0fffe5f06b723b3e0
Hasindu <hlrathnayaka@gmail.com>
2020-05-23 13:40:52 +0530

    ConsoleUI is now a beast!

--- Methane.Toolkit/UniUI/ConsoleCLI.cs
+++ Methane.Toolkit/UniUI/ConsoleCLI.cs
@@ -12,15 +12,63 @@ namespace UniUI
         }
 
 
+        readonly StringBuilder Cache = new StringBuilder();
+        int CacheSize = 0;
+        public int MaxCacheSize = 10000;
 
         public void Log(string s)
         {
-            Console.WriteLine(s);
+            CacheSize++;
+            while (LazyUpdating && CacheSize > MaxCacheSize)
+                System.Threading.Thread.Sleep(100);
+
+            lock (Cache)
+            {
+                Cache.AppendLine(s);
+            }
+
+            if (!LazyUpdating) new System.Threading.Thread(LazyUpdate).Start();
         }
 
         public void LogAppend(string s, int numberOfLines = 0)
         {
+            CacheSize += numberOfLines;
+            while (LazyUpdating && CacheSize > MaxCacheSize)
+                System.Threading.Thread.Sleep(100);
+
+            lock (Cache)
+            {
+                Cache.Append(s);
+            }
+
+            if (!LazyUpdating) new System.Threading.Thread(LazyUpdate).Start();
+        }
+
+
+        bool LazyUpdating = false;
+        void LazyUpdate()
+        {
+            if (LazyUpdating) return;
+
+            LazyUpdating = true;
+
+            if (CacheSize < MaxCacheSize)
+                System.Threading.Thread.Sleep(500);
+
+
+            string s;
+            lock (Cache)
+            {
+                s = Cache.ToString();
+                Cache.Clear();
+                CacheSize = 0;
+            }
+
             Console.Write(s);
+
+
+            LazyUpdating = false;
+
         }
 
 
